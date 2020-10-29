ab99233d8512c169f872c488b1b2c0d7373a4bc1
Technote <technote.space@gmail.com>
2020-10-30 02:46:49 +0900

    fix: consider brackets

--- __tests__/transform.test.ts
+++ __tests__/transform.test.ts
@@ -112,7 +112,7 @@ describe('transform', () => {
       '## API ##',
     ].join('\n'),
     ['**Table of Contents**  *generated with [DocToc](https://github.com/technote-space/doctoc)*\n\n',
-      '- [My Module !"&#035;$%&\'()=~¥@[];:*+{}](#my-module-)\n',
+      '- [My Module !"&#035;$%&\'()=~¥@&#91;&#93;;:*+{}](#my-module-)\n',
       '  - [API](#api)\n\n\n',
     ].join(''),
   );
