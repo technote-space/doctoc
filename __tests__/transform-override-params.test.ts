56c3140be1e1957e9f9534ac90e08f9fed9e876e
Technote <technote.space@gmail.com>
2021-01-17 15:38:13 +0900

    chore: add test pattern

--- __tests__/transform-override-params.test.ts
+++ __tests__/transform-override-params.test.ts
@@ -4,8 +4,8 @@ import {readFileSync} from 'fs';
 import {transform} from '../src';
 
 describe('transform', () => {
-  it('override params', () => {
-    const content = readFileSync(resolve(__dirname, 'fixtures/readme-with-params.md'), 'utf8');
+  it('override params (empty toc)', () => {
+    const content = readFileSync(resolve(__dirname, 'fixtures/readme-with-params1.md'), 'utf8');
     const headers = transform(content, {isNotitle: true, isFolding: false, maxHeaderLevel: 1, entryPrefix: '*'});
 
     expect(headers.wrappedToc.split('\n')).toEqual(
@@ -34,8 +34,38 @@ describe('transform', () => {
     );
   });
 
+  it('override params (not empty toc)', () => {
+    const content = readFileSync(resolve(__dirname, 'fixtures/readme-with-params2.md'), 'utf8');
+    const headers = transform(content, {isNotitle: true, isFolding: false, maxHeaderLevel: 1, entryPrefix: '*'});
+
+    expect(headers.wrappedToc.split('\n')).toEqual(
+      [
+        '<!-- START doctoc generated TOC please keep comment here to allow auto update -->',
+        '<!-- DON\'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->',
+        '<!-- param::entryPrefix::-:: -->',
+        '<!-- param::mode::github.com:: -->',
+        '<!-- param::isNotitle::false:: -->',
+        '<!-- param::maxHeaderLevel::8:: -->',
+        '**Test title**',
+        '',
+        '- [Installation](#installation)',
+        '- [API](#api)',
+        '    - [dockops::Containers(docker) → {Object}](#dockopscontainersdocker-%E2%86%92-object)',
+        '      - [Parameters:](#parameters)',
+        '      - [Returns:](#returns)',
+        '    - [dockops::Containers::activePorts(cb)](#dockopscontainersactiveportscb)',
+        '      - [Parameters:](#parameters-1)',
+        '    - [dockops::Containers::clean(id, cb)](#dockopscontainerscleanid-cb)',
+        '      - [Parameters:](#parameters-2)',
+        '- [^License](#license)',
+        '',
+        '<!-- END doctoc generated TOC please keep comment here to allow auto update -->',
+      ],
+    );
+  });
+
   it('not override params', () => {
-    const content = readFileSync(resolve(__dirname, 'fixtures/readme-with-params.md'), 'utf8');
+    const content = readFileSync(resolve(__dirname, 'fixtures/readme-with-params1.md'), 'utf8');
     const headers = transform(content, {isFolding: true, title: 'Test title'});
 
     expect(headers.wrappedToc.split('\n')).toEqual(
